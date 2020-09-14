ee3a6da8d3edffc3d616d229bbd14de121324a6d
jba1989git <jba1989@gmail.com>
2020-09-14 23:11:43 +0800

    add 搬移User Model

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,6 +62,11 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            .top-right > a {
+                font-size: 22px;
+                color: red;
+            }
         </style>
     </head>
     <body>
@@ -80,15 +85,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                </div>                
             </div>
         </div>
     </body>
